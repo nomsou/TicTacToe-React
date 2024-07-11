fb33d56edbbfa342c1ec2911f870096aa71dfe6d
Nomso <nomsounaogu@gmail.com>
2024-07-11 11:21:34 +0100

    modified:   index.html 	deleted:    src/App.css 	modified:   src/App.jsx 	new file:   src/Board.jsx 	new file:   src/Players.jsx 	new file:   src/css/App.css 	new file:   src/css/board.css 	new file:   src/css/players.css 	deleted:    src/index.css 	new file:   src/main.css 	modified:   src/main.jsx

---
+++ src/Board.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './css/board.css'
+
+const Board = () => {
+  return (
+    <div className="board">
+      <div className="input input1"></div>
+      <div className="input input2"></div>
+      <div className="input input3"></div>
+      <div className="input input4"></div>
+      <div className="input input5"></div>
+      <div className="input input6"></div>
+      <div className="input input7"></div>
+      <div className="input input8"></div>
+      <div className="input input9"></div>
+    </div>
+  )
+}
+
+export default Board
