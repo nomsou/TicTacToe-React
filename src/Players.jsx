c7c73bd88aed768669e55feb94f25468f35583c1
Nomso <nomsounaogu@gmail.com>
2024-07-11 12:58:19 +0100

    Changed 0 to O

--- src/Players.jsx
+++ src/Players.jsx
@@ -5,7 +5,7 @@ const Players = () => {
   return (
     <div className='players'>
       <div className="player">Player 1: X</div>
-      <div className="player">Player 2: 0</div>
+      <div className="player">Player 2: O</div>
     </div>
   )
 }
